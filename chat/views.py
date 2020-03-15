289d88d385012efc90377cd11e440d3004dfbf63
lolo0159228 <lolo0159228@gmail.com>
2020-03-15 23:03:52 +0800

    css對話部分未修改完

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, HttpResponse
 from dwebsocket.decorators import accept_websocket
 from django.views.decorators.csrf import csrf_exempt
 import uuid
+import json
 
 OnlineUsers = {}
 
@@ -27,20 +28,28 @@ def chat(request):
                 break
             else:
                 print('USER連接成功')
-                msg = '新成員->' + username + '  加入聊天室'
-                for user in OnlineUsers:
-                    OnlineUsers[user].send(msg.encode('utf-8'))
-                OnlineUsers[username] = request.websocket
-
-# @accept_websocket
-# def connect(request):
-#
-
-
+                print(message.decode())  #message是bytes type
+                if message.decode() == 'connected':
+                    msg = {
+                        'id':username,
+                        'text': '新成員->' + username + '  加入聊天室',
+                        'type': 0
+                    }
+                    #user第一次進入聊天室
+                    request.websocket.send(json.dumps(msg).encode('utf-8'))
+                    #通知所有user有新成員進入
+                    msg['type'] = 1
+                    for user in OnlineUsers:
+                        OnlineUsers[user].send(json.dumps(msg).encode('utf-8'))
+                    OnlineUsers[username] = request.websocket
 
 @csrf_exempt
 def send(request):
-    msg = request.POST.get('msg')
+    msg = {
+        'from': request.POST.get('from'),
+        'text': request.POST.get('msg'),
+        'type': 2
+    }
     for user in OnlineUsers:
-        OnlineUsers[user].send(msg.encode('utf-8'))
+        OnlineUsers[user].send(json.dumps(msg).encode('utf-8'))
     return HttpResponse({'msg':'成功發送訊息給所有user'})
